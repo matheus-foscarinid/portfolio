7e3b2d490c0dcd1275ba1066ab980cc4cf7ffd3a
Matheus Dias <matheus.foscarinid@gmail.com>
2023-04-24 19:24:57 -0300

    chore(Views/Career): testing the career view

--- src/components/views/CareerView.vue
+++ src/components/views/CareerView.vue
@@ -1,11 +1,27 @@
 <template>
   <section id="career">
-    <div>Career</div>
+    <div>
+      <h1>Career</h1>
+
+      <div class="places-container">
+        <button
+          v-for="(place, index) in placesTimeline"
+          :key="index"
+        >
+          {{ place.name }}
+        </button>
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
-
+  const placesTimeline = [
+    { name: 'CIMOL', description: '' },
+    { name: 'Unisinos', description: '' },
+    { name: 'Scopi', description: '' },
+    { name: 'Minha visita', description: '' }
+  ]
 </script>
 
 <style lang="scss" scoped>
@@ -14,5 +30,28 @@
     display: flex;
     justify-content: center;
     flex-direction: column;
+
+    .places-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      flex-wrap: wrap;
+      max-width: 300px;
+
+      button {
+        margin: 0 10px 10px 0;
+        padding: 10px 20px;
+        border: 1px solid #000;
+        border-radius: 5px;
+        background-color: #fff;
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+
+        &:hover {
+          background-color: #000;
+          color: #fff;
+        }
+      }
+    }
   }
 </style>
