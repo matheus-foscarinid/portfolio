74c4fb2422d5cce2da5f215dc7db0eaa0a1879f2
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-04 18:35:00 -0300

    feat(CareerView): adding the buttons and content initial structure

--- src/components/views/CareerView.vue
+++ src/components/views/CareerView.vue
@@ -4,24 +4,44 @@
       <h1>Career</h1>
 
       <div class="places-container">
-        <button
-          v-for="(place, index) in placesTimeline"
-          :key="index"
-        >
-          {{ place.name }}
-        </button>
+        <div class="buttons-container">
+          <div 
+            class="current-selected-marker"
+            :style="{ top: `${currentPlace * 25}%` }"
+          />
+          <button
+            v-for="(place, index) in placesTimeline"
+            :key="index"
+            :class="{ active: currentPlace === index }"
+            @click="setPlaceAsActive(index)"
+          >
+            {{ place.name }}
+          </button>
+        </div>
+
+        <div class="active-place-content">
+          {{ placesTimeline[currentPlace].description }}
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+
+  const currentPlace = ref(0);
+
   const placesTimeline = [
-    { name: 'CIMOL', description: '' },
-    { name: 'Unisinos', description: '' },
-    { name: 'Scopi', description: '' },
-    { name: 'Minha visita', description: '' }
+    { name: 'CIMOL', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc magna, porta id cursus quis, molestie in leo. Nam condimentum, velit non dapibus molestie, libero nisl imperdiet ipsum, eget interdum est ligula at enim. Proin ac tristique metus, nec porttitor justo. Cras scelerisque risus turpis, sed aliquet risus aliquam vitae. Etiam ac dictum ex. Sed mattis pharetra malesuada. Cras bibendum dolor id consectetur facilisis. Quisque feugiat est a facilisis maximus. Duis vel venenatis quam, in auctor nisl. Phasellus sit amet nunc id lorem molestie bibendum. Donec sit amet eleifend metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent egestas dictum dui, sit amet accumsan leo elementum quis. Aenean consectetur ultricies aliquam. Pellentesque eget maximus lorem, sit amet sollicitudin dui. Nullam tempor sem vitae nibh malesuada, id pellentesque eros iaculis.' },
+    { name: 'Unisinos', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc magna, porta id cursus quis, molestie in leo. Nam condimentum, velit non dapibus molestie, libero nisl imperdiet ipsum, eget interdum est ligula at enim. Proin ac tristique metus, nec porttitor justo. Cras scelerisque risus turpis, sed aliquet risus aliquam vitae. Etiam ac dictum ex. Sed mattis pharetra malesuada. Cras bibendum dolor id consectetur facilisis. Quisque feugiat est a facilisis maximus. Duis vel venenatis quam, in auctor nisl. Phasellus sit amet nunc id lorem molestie bibendum. Donec sit amet eleifend metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent egestas dictum dui, sit amet accumsan leo elementum quis. Aenean consectetur ultricies aliquam. Pellentesque eget maximus lorem, sit amet sollicitudin dui. Nullam tempor sem vitae nibh malesuada, id pellentesque eros iaculis.' },
+    { name: 'Scopi', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc magna, porta id cursus quis, molestie in leo. Nam condimentum, velit non dapibus molestie, libero nisl imperdiet ipsum, eget interdum est ligula at enim. Proin ac tristique metus, nec porttitor justo. Cras scelerisque risus turpis, sed aliquet risus aliquam vitae. Etiam ac dictum ex. Sed mattis pharetra malesuada. Cras bibendum dolor id consectetur facilisis. Quisque feugiat est a facilisis maximus. Duis vel venenatis quam, in auctor nisl. Phasellus sit amet nunc id lorem molestie bibendum. Donec sit amet eleifend metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent egestas dictum dui, sit amet accumsan leo elementum quis. Aenean consectetur ultricies aliquam. Pellentesque eget maximus lorem, sit amet sollicitudin dui. Nullam tempor sem vitae nibh malesuada, id pellentesque eros iaculis.' },
+    { name: 'Minha visita', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc magna, porta id cursus quis, molestie in leo. Nam condimentum, velit non dapibus molestie, libero nisl imperdiet ipsum, eget interdum est ligula at enim. Proin ac tristique metus, nec porttitor justo. Cras scelerisque risus turpis, sed aliquet risus aliquam vitae. Etiam ac dictum ex. Sed mattis pharetra malesuada. Cras bibendum dolor id consectetur facilisis. Quisque feugiat est a facilisis maximus. Duis vel venenatis quam, in auctor nisl. Phasellus sit amet nunc id lorem molestie bibendum. Donec sit amet eleifend metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent egestas dictum dui, sit amet accumsan leo elementum quis. Aenean consectetur ultricies aliquam. Pellentesque eget maximus lorem, sit amet sollicitudin dui. Nullam tempor sem vitae nibh malesuada, id pellentesque eros iaculis.' }
   ]
+
+  const setPlaceAsActive = (index) => {
+    currentPlace.value = index;
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -33,25 +53,55 @@
 
     .places-container {
       display: flex;
-      flex-direction: column;
       justify-content: center;
+      align-items: start;
       flex-wrap: wrap;
-      max-width: 300px;
+      gap: 0.5rem;
+      margin-top: 2rem;
+      
 
-      button {
-        margin: 0 10px 10px 0;
-        padding: 10px 20px;
-        border: 1px solid #000;
-        border-radius: 5px;
-        background-color: #fff;
-        cursor: pointer;
-        transition: all 0.3s ease-in-out;
+      & .buttons-container {
+        display: flex;
+        flex-direction: column;
+        
+        button {
+          margin: 0;
+          padding: 1rem;
+          border: 0;
+          border-left: 2px solid var(--disabled-text);
+          background: transparent;
+          cursor: pointer;
+          transition: all 0.2s ease-in-out;
+          color: var(--disabled-text);
+          font-weight: 600;
 
-        &:hover {
-          background-color: #000;
-          color: #fff;
+          &:hover {
+            background-color: var(--default-border);
+          }
+
+          &.active {
+            background-color: var(--default-border);
+            color: var(--active-text);
+          }
+        }
+
+        .current-selected-marker {
+          width: 3px;
+          height: 25%;
+          background-color: var(--details-background);
+          position: absolute;
+          left: 0;
+          top: 0;
+          transition: all 0.3s ease-in-out;
+          z-index: 10;
         }
       }
+
+      & .active-place-content {
+        flex: 1;
+        padding: 2rem;
+      }
+
     }
   }
 </style>
