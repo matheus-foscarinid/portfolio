a3ab6c402ab20c656f2fb64719e9797069b2e727
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-05 19:57:10 -0300

    chore(ProjectsView): adding the project view structure

--- src/components/views/ProjectsView.vue
+++ src/components/views/ProjectsView.vue
@@ -1,17 +1,88 @@
 <template>
   <section id="projects">
+    <div class="container">
+      <h2>Projects</h2>
+
+      <div class="project-cards">
+
+        <ProjectCard
+          v-for="(project, index) in projects"
+          :key="index"
+          :project="project"
+          :reverse="index % 2 === 0"
+        />
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
+  import ProjectCard from '@/components/cards/ProjectCard.vue';
 
+
+  const projects = [
+    {
+      link: 'test.co',
+      name: 'Test',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+    },
+    {
+      link: 'test.co',
+      name: 'Test',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+    },
+    {
+      link: 'test.co',
+      name: 'Test',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+    },
+  ]
 </script>
 
-<style>
+<style lang="scss" scoped>
   #projects {
     height: 100vh;
     display: flex;
     justify-content: center;
     flex-direction: column;
+
+    h2 {
+      display: flex;
+      align-items: center;
+      font-size: 1.5rem;
+      font-weight: 700;
+      margin-bottom: 1rem;
+      color: var(--secondary-text);
+
+      &::before {
+      content: '';
+      display: block;
+      position: relative;
+      width: 50%;
+      height: 1px;
+      margin-right: 20px;
+      background-color: var(--default-border);
+    }
+
+      &::after {
+        content: '';
+        display: block;
+        position: relative;
+        width: 50%;
+        height: 1px;
+        margin-left: 20px;
+        background-color: var(--default-border);
+      }
+    }
+
+    .project-cards {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: start;
+      flex-wrap: wrap;
+      gap: 2rem;
+      margin-top: 2rem;
+    }
   }
 </style>
