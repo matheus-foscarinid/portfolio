8f171ffdbabd67313aa7ea69fdd89991ee84ee4d
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-09 19:32:40 -0300

    chore(FontAwesome): configuring FA and its icons

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faArrowUpRightFromSquare, faFolder } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faGithub, faArrowUpRightFromSquare, faFolder)
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
