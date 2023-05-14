454515c812cd4da0ab127e2c3fbdfae97e43d1f0
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-14 14:47:23 -0300

    feat(ContactView): adding the contact cards with content

--- src/components/views/ContactView.vue
+++ src/components/views/ContactView.vue
@@ -1,18 +1,82 @@
 <template>
   <section id="contact">
-    <div>Contact</div>
+    <div class="container">
+      <h2>Contact</h2>
+
+      <div class="contact-cards">
+        <ContactCard
+          v-for="(contact, index) in contacts"
+          :key="index"
+          :contact="contact"
+        />
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
+  import ContactCard from '@/components/cards/ContactCard.vue';
 
+  const contacts = [
+    {
+      icon: 'fa-solid fa-location-pin',
+      text: 'Rio Grande do Sul, Brazil',
+    },
+    {
+      icon: 'fa-solid fa-envelope',
+      text: 'matheus.foscarinid@gmail.com',
+    },
+    {
+      icon: 'fa-brands fa-github',
+      text: 'matheus-foscarinid',
+    },
+    {
+      icon: 'fa-brands fa-linkedin',
+      text: 'Matheus Foscarini Dias',
+    },
+  ];
 </script>
 
-<style>
+<style lang="scss">
   #contact {
-    height: 100vh;
+    height: 60vh;
     display: flex;
     justify-content: center;
     flex-direction: column;
+    background: white;
+
+    h2 {
+      display: flex;
+      align-items: center;
+      font-size: 1.5rem;
+      font-weight: 700;
+      margin-bottom: 1rem;
+      color: var(--secondary-text);
+
+      &::before {
+        content: '';
+        display: block;
+        position: relative;
+        width: 50px;
+        height: 1px;
+        margin-right: 20px;
+        background-color: var(--default-border);
+      }
+      &::after {
+        content: '';
+        display: block;
+        position: relative;
+        width: 530px;
+        height: 1px;
+        margin-left: 20px;
+        background-color: var(--default-border);
+      }
+    }
+
+    .contact-cards {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
   }
 </style>
