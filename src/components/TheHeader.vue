924043ebd2ba33e75b8e84228520f01f20d8169f
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-18 09:42:39 -0300

    chore(TheHeader): hiding the link to projects

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,7 +7,7 @@
     <ul class="menu">
       <li><a href="#about">About</a></li>
       <li><a href="#career">Career</a></li>
-      <li><a href="#projects">Projects</a></li>
+      <!-- <li><a href="#projects">Projects</a></li> -->
       <li><a href="#contact">Contact</a></li>
     </ul>
   </header>
