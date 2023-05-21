7a271ebe46eb8579d1cab1384c89eaf9a430c6a5
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-21 20:25:37 -0300

    chore(icons): adding the close icon

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ import {
   faFolder, 
   faEnvelope, 
   faLocationPin, 
-  faBars 
+  faBars ,
+  faClose
 } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
@@ -21,7 +22,8 @@ library.add(
   faFolder, 
   faEnvelope, 
   faLocationPin,
-  faBars
+  faBars,
+  faClose
 )
 
 createApp(App)
