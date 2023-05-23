1759cd4f77c58b5cf81f1727bb225c5f0183b52b
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-23 14:26:09 -0300

    feat(TheHeader): adding the home button

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,6 +5,7 @@
     </div>
 
     <ul class="menu">
+      <li><a href="#home">Home</a></li>
       <li><a href="#about">About</a></li>
       <li><a href="#career">Career</a></li>
       <!-- <li><a href="#projects">Projects</a></li> -->
@@ -38,6 +39,7 @@
           class="mobile-menu-items"
           @click="closeMobileMenu"
         >
+          <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#career">Career</a></li>
           <!-- <li><a href="#projects">Projects</a></li> -->
