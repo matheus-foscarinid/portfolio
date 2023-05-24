d21ecf4522f2f049f31dcdbdf005a06a03d75bab
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-24 18:48:43 -0300

    feat(TheHeader): using the ThemeSwitch on desktop

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -12,6 +12,10 @@
       <li><a href="#contact">Contact</a></li>
     </ul>
 
+    <div class="theme-switch-container">
+      <ThemeSwitch />
+    </div>
+
     <div class="mobile-menu-icon">
       <font-awesome-icon 
         icon="fas fa-bars"
@@ -42,7 +46,6 @@
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#career">Career</a></li>
-          <!-- <li><a href="#projects">Projects</a></li> -->
           <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
@@ -62,6 +65,8 @@
 </template>
 
 <script setup>
+  import ThemeSwitch from './ThemeSwitch.vue';
+
   import { ref } from 'vue'
   const isMenuOpen = ref(false);
 
