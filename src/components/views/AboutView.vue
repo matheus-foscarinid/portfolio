09495c997fb7b6e1c4738ec2ecabcacbdfd5fb05
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-29 14:55:06 -0300

    chore(AboutView): adding the translations to the about view

--- src/components/views/AboutView.vue
+++ src/components/views/AboutView.vue
@@ -4,31 +4,10 @@
       <h2>About</h2>
       <div class="content-containers">
         <div class="summary">
-          <p>
-            Hey there, I'm <span class="highlight">Matheus</span>, a fullstack developer based
-            in Rio Grande do Sul, Brazil. For me, creating digital products is more than just a job
-            - it's a passion. I first discovered this passion while dabbling in graphic design using
-            Photoshop, which eventually led me to explore the exciting world of web development.
-          </p>
-          <p>
-            With <span class="highlight">3 years of experience</span> under my belt, I'm always
-            eager to learn and expand my skillset. I'm particularly interested in
-            <span class="highlight">design patterns and principles</span>, and strive to apply them
-            in all of my work.
-          </p>
-          <p>
-            My journey began as a <span class="highlight">trainee working with Ruby on Rails</span>,
-            and I've since broadened my horizons to include technologies such as Vue and NestJS. I
-            love the challenge of creating
-            <span class="highlight">user-centric applications</span> that solve real-world problems,
-            and I take great pride in delivering products that are not only functional, but also
-            visually appealing.
-          </p>
-          <p>
-            When I'm not coding, you can usually find me keeping up-to-date with the
-            <span class="highlight">latest industry trends</span> or experimenting with new tools
-            and frameworks. Thanks for stopping by and getting to know me a little better!
-          </p>
+          <p v-html="$t('ABOUT.SUMMARY_1')" />
+          <p v-html="$t('ABOUT.SUMMARY_2')" />
+          <p v-html="$t('ABOUT.SUMMARY_3')" />
+          <p v-html="$t('ABOUT.SUMMARY_4')" />
         </div>
 
         <div class="tech-stack">
@@ -106,11 +85,6 @@ const stacks = [
 
     .summary {
       max-width: 700px;
-
-      .highlight {
-        color: var(--primary-text);
-        font-weight: 700;
-      }
     }
 
     .tech-stack {
