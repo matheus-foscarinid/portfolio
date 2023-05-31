67da53f0ff4e2c02535db0eadafb67def819f0c2
Matheus Dias <matheus.foscarinid@gmail.com>
2023-05-31 16:29:06 -0300

    chore(i18n): disabling legacy mode

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,6 +6,7 @@ import { createI18n } from 'vue-i18n';
 const locale = window.navigator.language.substring(0, 2);
 
 const i18n = createI18n({
+  legacy: false,
   locale,
   fallbackLocale: 'en',
   messages: { pt, en }
