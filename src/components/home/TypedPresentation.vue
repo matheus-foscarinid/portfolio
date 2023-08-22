0cf3a884a7b7ac5be981a24dabacaf37428b1710
Matheus F. Dias <matheus.foscarinid@gmail.com>
2023-08-22 19:01:32 -0300

    feat(TypedPresentation): adding messages and correct styling

--- src/components/home/TypedPresentation.vue
+++ src/components/home/TypedPresentation.vue
@@ -1,64 +1,84 @@
 <template>
-  <div class="container">
-    <h1>I'm a
-      <span class="typed-text">{{ typeValue }}</span>
-      <span class="cursor" :class="{'typing': typeStatus}">&nbsp;</span>
-    </h1>
-  </div>
+  <h3>
+    I'm a <span class="typed-text">{{ typedValue }}</span>
+    <span class="cursor" :class="{'typing': isTyping}">|</span>
+  </h3>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-const typeValue = ref('');
-const typeStatus = ref(false);
+const typedValue = ref('');
+const isTyping = ref(false);
 
-const typeArray = ['developer', 'designer', 'animals lover', 'teacher'];
+const titlesArray = ['Fullstack developer', 'Designer', 'Cat dad', 'Teacher'];
 
-// constants
-const TYPING_SPEED = 200;
-const ERASING_SPEED = 100;
-const NEW_TEXT_DELAY = 2000;
+const TYPING_SPEED = 30;
+const ERASING_SPEED = 30;
+const NEW_TEXT_DELAY = 3500;
 
-// Indexes
-const typeArrayIndex = 0;
-const charIndex = 0;
+let titlesArrayIndex = 0;
+let charIndex = 0;
 
+const typeText = () => {
+  const currentWord = titlesArray[titlesArrayIndex];
+
+  if(charIndex < currentWord.length) {
+    if(!isTyping.value) isTyping.value = true;
+
+    typedValue.value += currentWord.charAt(charIndex);
+    charIndex += 1;
+
+    setTimeout(typeText, TYPING_SPEED);
+  } else {
+    isTyping.value = false;
+    setTimeout(eraseText, NEW_TEXT_DELAY);
+  }
+};
+
+const eraseText = () => {
+  if(charIndex > 0) {
+    if(!isTyping.value) isTyping.value = true;
+
+    typedValue.value = titlesArray[titlesArrayIndex].substring(0, charIndex - 1);
+    charIndex -= 1;
+    setTimeout(eraseText, ERASING_SPEED);
+  } else {
+    isTyping.value = false;
+    titlesArrayIndex += 1;
+    if(titlesArrayIndex >= titlesArray.length) titlesArrayIndex = 0;
+
+    setTimeout(typeText, TYPING_SPEED);
+  }
+}
+
+onMounted(() => setTimeout(typeText, 1000));
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  h3 {
+    color: var(--secondary-text);
+    font-size: 2.5rem;
+    margin: 0;
 
-  h1 {
-    font-size: 6rem;
-    font-weight: normal;
-
-    span.typed-text {
-      color: #D2B94B;
+    & .typed-text {
+      font-weight: 700;
     }
-
-    span.cursor {
+    & .cursor {
+      content: "&nbsp;";
       display: inline-block;
-      margin-left: 3px;
-      width: 4px;
-      background-color: #fff;
+      margin-left: 1px;
       animation: cursorBlink 1s infinite;
-    }
 
-    span.cursor.typing {
-      animation: none;
+      &.typing {
+        animation: none;
+      }
     }
   }
 
   @keyframes cursorBlink {
-    49% { background-color: #fff; }
-    50% { background-color: transparent; }
-    99% { background-color: transparent; }
+    49% { opacity: 1; }
+    50% { opacity: 0; }
+    99% { opacity: 0; }
   }
 </style>
