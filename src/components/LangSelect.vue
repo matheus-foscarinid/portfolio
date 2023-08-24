7a27cd910a9fe7a4861fd77db21b6f4a99b91b4c
Matheus F. Dias <matheus.foscarinid@gmail.com>
2023-08-24 19:00:55 -0300

    feat(LangSelect): adding the new custom lang select with flags and icons

--- src/components/LangSelect.vue
+++ src/components/LangSelect.vue
@@ -1,47 +1,146 @@
 <template>
-  <div class="lang-select">
-    <select v-model="lang" @change="changeLang">
-      <option v-for="lang in langs" :key="lang.value" :value="lang.value">
-        {{ lang.label }}
-      </option>
-    </select>
+  <div 
+    class="lang-select" 
+    tabindex="0"
+    @blur="isDropdownOpen = false"
+  >
+    <div 
+      class="selected-option"
+      :class="{ 'active': isDropdownOpen }"
+      @click="toggleDropdown"
+    >
+      <img :src="selectedLang.img" alt="lang" />
+      {{ selectedLang.label }}
+
+      <font-awesome-icon 
+        icon="fas fa-caret-down"
+        class="arrow"
+        :class="{ rotated: isDropdownOpen }"
+      />
+    </div>
+
+    <Transition 
+      name="fade"
+      mode="out-in"
+    >
+      <div
+        v-if="isDropdownOpen"
+        class="options"
+      >
+        <div v-for="lang in langs" :key="lang.value" @click="selectLang(lang.value)">
+          <img :src="lang.img" alt="lang" />
+          {{ lang.label }}
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, computed } from 'vue';
 import i18n from '../i18n';
+
+import ptFlagImg from '@/assets/images/pt.png';
+import enFlagImg from '@/assets/images/en.png';
+
+const isDropdownOpen = ref(false);
+const lang = ref(i18n.global.locale)
+
+const selectedLang = computed(() => {
+  return langs.find(({ value }) => value === lang.value);
+});
 
 const langs = [
   {
     label: 'English',
-    value: 'en'
+    value: 'en',
+    img: enFlagImg
   }, 
   {
     label: 'Português',
-    value: 'pt'
+    value: 'pt',
+    img: ptFlagImg
   }
 ];
-const lang = ref(i18n.global.locale)
 
-watch(lang, (newLang) => {
+const selectLang = (newLang) => {
+  console.log(newLang);
+  lang.value = newLang;
   i18n.global.locale = newLang;
-})
+  isDropdownOpen.value = false;
+};
 
+const toggleDropdown = () => {
+  isDropdownOpen.value = !isDropdownOpen.value;
+};
 </script>
 
 <style lang="scss" scoped>
-.lang-select {
-  select {
-    padding: 0.5rem;
+  .lang-select {
+    position: relative;
+    cursor: pointer;
+  }
+
+  img {
+    width: 1.2rem;
+    height: 1.2rem;
+  }
+
+  .selected-option {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem 1rem;
     border-radius: 0.5rem;
-    border: 1px solid var(--default-border);
     background-color: var(--details-background);
     color: var(--default-text);
     font-size: 1rem;
     font-family: inherit;
-    cursor: pointer;
     outline: none;
+    box-shadow: 0 0 3px 3px rgba(0, 0, 0, 0.05);
+    gap: .5rem;
+    transition: box-shadow 0.2s ease-in-out;
+
+    &.active {
+      border-radius: 0.5rem 0.5rem 0 0;
+    }
+
+    &:hover {
+      box-shadow: 0 0 7px 3px rgba(0, 0, 0, 0.07);
+    }
   }
-}
+
+  .arrow {
+    align-self: center;
+    font-size: 0.8rem;
+    transition: transform 0.2s ease-in-out;
+
+    &.rotated {
+      transform: rotate(180deg);
+    }
+  }
+
+
+  .options {
+    display: block;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    overflow-y: auto;
+    background-color: var(--details-background);
+    border-radius: 0 0 .5rem .5rem;
+    border-top: 1px solid var(--default-border);
+
+    & > div {
+      display: flex;
+      align-items: center;
+      gap: .5rem;
+      padding: 0.5rem 1rem;
+      cursor: pointer;
+
+      &:hover {
+        background-color: var(--secondary-background);
+      }
+    }
+  }
 </style>
