df822cd509667c26d66813dac5832ea3f14b2a4c
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2025-10-27 20:24:19 -0300

    feat: update career view

--- src/components/views/CareerView.vue
+++ src/components/views/CareerView.vue
@@ -5,10 +5,7 @@
 
       <div class="places-container">
         <div class="buttons-container">
-          <div 
-            class="current-selected-marker"
-            :style="selectedMarkerStyle"
-          />
+          <div class="current-selected-marker" :style="selectedMarkerStyle" />
           <button
             v-for="(place, index) in placesTimeline"
             :key="index"
@@ -19,21 +16,11 @@
           </button>
         </div>
 
-        <Transition 
-          name="fade"
-          mode="out-in"
-        >
-          <div 
-            class="active-place-content"
-            :key="currentPlaceIndex"
-          >
+        <Transition name="fade" mode="out-in">
+          <div class="active-place-content" :key="currentPlaceIndex">
             <div class="title">
               <span>{{ currentPlace.role }} at </span>
-              <a 
-                class="place-name"
-                :href="currentPlace.link"
-                target="_blank"
-              >
+              <a class="place-name" :href="currentPlace.link" target="_blank">
                 {{ currentPlace.name }}
               </a>
             </div>
@@ -43,10 +30,7 @@
             </div>
 
             <ul class="description">
-              <li 
-                v-for="i in currentPlace.paragraphsQtt"
-                :key="i"
-              >
+              <li v-for="i in currentPlace.paragraphsQtt" :key="i">
                 {{ $t(`CAREER.${currentPlace.key}.RESUME_${i}`) }}
               </li>
             </ul>
@@ -58,135 +42,143 @@
 </template>
 
 <script setup>
-  import { gsap } from 'gsap';
+import { gsap } from 'gsap'
 
-  import { ref, computed, onMounted } from 'vue';
-  import { useI18n } from 'vue-i18n';
+import { ref, computed, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-  const { t: $t } = useI18n();
+const { t: $t } = useI18n()
 
-  const currentPlaceIndex = ref(0);
-  const currentPlace = computed(() => (placesTimeline[currentPlaceIndex.value] || {}));
+const currentPlaceIndex = ref(0)
+const currentPlace = computed(() => placesTimeline[currentPlaceIndex.value] || {})
 
-  const selectedMarkerStyle = computed(() => {
-    // If is mobile
-    if (window.innerWidth <= 768) {
-      return {
-        left: `${currentPlaceIndex.value * 20}%`,
-      };
-    }
-
+const selectedMarkerStyle = computed(() => {
+  // If is mobile
+  if (window.innerWidth <= 768) {
     return {
-      top: `${currentPlaceIndex.value * 20}%`,
-    };
-  });
-
-  const placesTimeline = [
-    { 
-      name: 'CIMOL',
-      key: 'CIMOL',
-      paragraphsQtt: 3,
-      role: 'Student',
-      period: 'from 2018 to 2020',
-      link: 'https://www.cimol.g12.br/',
-   },
-    { 
-      name: 'Unisinos',
-      key: 'UNISINOS',
-      paragraphsQtt: 2,
-      role: 'Student',
-      period: 'from 2021 until now',
-      link: 'https://www.unisinos.br/',
-    },
-    { 
-      name: 'Scopi',
-      key: 'SCOPI',
-      paragraphsQtt: 3,
-      role: 'Trainee',
-      period: 'from January 2021 to October 2021',
-      link: 'https://www.scopi.com.br/',
-    },
-    { 
-      name: 'Minha visita',
-      key: 'MINHA_VISITA',
-      paragraphsQtt: 3,
-      role: 'Fullstack Mid Developer',
-      period: 'from October 2021 to November 2024',
-      link: 'https://www.minhavisita.app/',
-    },
-    {
-      name: 'Fullstack Labs',
-      key: 'FULLSTACK_LABS',
-      paragraphsQtt: 3,
-      role: 'Mid-level Software Engineer',
-      period: 'from November 2024 to present',
-      link: 'https://www.fullstack.com/',
+      left: `${currentPlaceIndex.value * (100 / placesTimeline.length)}%`
     }
-  ]
-
-  const setPlaceAsActive = (index) => {
-    currentPlaceIndex.value = index;
   }
 
-  const addObserverOnScroll = () => {
-    const observer = new IntersectionObserver((entries, self) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          animateElement();
-          self.unobserve(entry.target);
-        }
-      });
-    });
+  return {
+    top: `${currentPlaceIndex.value * (100 / placesTimeline.length)}%`
+  }
+})
 
-    const target = document.querySelector('#career .container');
-    observer.observe(target)
+const placesTimeline = [
+  {
+    name: 'CIMOL',
+    key: 'CIMOL',
+    paragraphsQtt: 3,
+    role: 'Student',
+    period: 'from 2018 to 2020',
+    link: 'https://www.cimol.g12.br/'
+  },
+  {
+    name: 'Unisinos',
+    key: 'UNISINOS',
+    paragraphsQtt: 2,
+    role: 'Student',
+    period: 'from 2021 until now',
+    link: 'https://www.unisinos.br/'
+  },
+  {
+    name: 'Scopi',
+    key: 'SCOPI',
+    paragraphsQtt: 3,
+    role: 'Trainee',
+    period: 'from January 2021 to October 2021',
+    link: 'https://www.scopi.com.br/'
+  },
+  {
+    name: 'Minha visita',
+    key: 'MINHA_VISITA',
+    paragraphsQtt: 3,
+    role: 'Fullstack Mid Developer',
+    period: 'from October 2021 to November 2024',
+    link: 'https://www.minhavisita.app/'
+  },
+  {
+    name: 'Fullstack Labs',
+    key: 'FULLSTACK_LABS',
+    paragraphsQtt: 3,
+    role: 'Mid-level Software Engineer',
+    period: 'from November 2024 to July 2025',
+    link: 'https://www.fullstack.com/'
+  },
+  {
+    name: 'HiPeople',
+    key: 'HIPEOPLE',
+    paragraphsQtt: 3,
+    role: 'Software Engineer',
+    period: 'from July 2025 to present',
+    link: 'https://www.hiprople.com/'
+  }
+]
 
-    function animateElement() {
-      const tl = gsap.timeline();
+const setPlaceAsActive = (index) => {
+  currentPlaceIndex.value = index
+}
 
-      const title = document.querySelector('#career h2');
-      const placesButtons = document.querySelectorAll('.buttons-container > *');
-      const placeInfos = document.querySelectorAll('.active-place-content > *');
+const addObserverOnScroll = () => {
+  const observer = new IntersectionObserver((entries, self) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        animateElement()
+        self.unobserve(entry.target)
+      }
+    })
+  })
 
-      gsap.fromTo(
-        title, 
-        { opacity: 0, y: 50, filter: 'blur(2px)', transition: 'none' }, 
-        { opacity: 1, y: 0, duration: .5, filter: 'blur(0px)', ease: 'back.out(1.4)' }
-      );
+  const target = document.querySelector('#career .container')
+  observer.observe(target)
 
-      tl.fromTo(
-        placesButtons, 
-        { opacity: 0, x: -50, filter: 'blur(2px)', transition: 'none' }, 
-        { opacity: 1, x: 0, duration: .5, filter: 'blur(0px)', stagger: .1 }
-      );
+  function animateElement() {
+    const tl = gsap.timeline()
 
-      gsap.fromTo(
-        placeInfos,
-        { opacity: 0, y: 20, transition: 'none' }, 
-        { opacity: 1, y: 0, duration: .5, stagger: .1 }
-      );
-    }
-  };
+    const title = document.querySelector('#career h2')
+    const placesButtons = document.querySelectorAll('.buttons-container > *')
+    const placeInfos = document.querySelectorAll('.active-place-content > *')
 
-  onMounted(addObserverOnScroll);
+    gsap.fromTo(
+      title,
+      { opacity: 0, y: 50, filter: 'blur(2px)', transition: 'none' },
+      { opacity: 1, y: 0, duration: 0.5, filter: 'blur(0px)', ease: 'back.out(1.4)' }
+    )
+
+    tl.fromTo(
+      placesButtons,
+      { opacity: 0, x: -50, filter: 'blur(2px)', transition: 'none' },
+      { opacity: 1, x: 0, duration: 0.5, filter: 'blur(0px)', stagger: 0.1 }
+    )
+
+    gsap.fromTo(
+      placeInfos,
+      { opacity: 0, y: 20, transition: 'none' },
+      { opacity: 1, y: 0, duration: 0.5, stagger: 0.1 }
+    )
+  }
+}
+
+onMounted(addObserverOnScroll)
 </script>
 
 <style lang="scss" scoped>
-  #career {
-    min-height: 90vh;
+#career {
+  min-height: 90vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  h2 {
     display: flex;
-    justify-content: center;
-    flex-direction: column;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+    color: var(--secondary-text);
 
-    h2 {
-      display: flex;
-      align-items: center;
-      font-size: 1.5rem;
-      font-weight: 700;
-      margin-bottom: 1rem;
-      color: var(--secondary-text);
-
-      &::before {
+    &::before {
       content: '';
       display: block;
       position: relative;
@@ -196,160 +188,161 @@
       background-color: var(--default-border);
     }
 
-      &::after {
-        content: '';
-        display: block;
-        position: relative;
-        width: 100%;
-        height: 1px;
-        margin-left: 20px;
-        background-color: var(--default-border);
+    &::after {
+      content: '';
+      display: block;
+      position: relative;
+      width: 100%;
+      height: 1px;
+      margin-left: 20px;
+      background-color: var(--default-border);
+    }
+  }
+
+  .places-container {
+    display: flex;
+    justify-content: center;
+    align-items: start;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    margin-top: 2rem;
+
+    & .buttons-container {
+      display: flex;
+      flex-direction: column;
+      position: relative;
+
+      button {
+        margin: 0;
+        margin-left: 1px;
+        padding: 1rem;
+        border: 0;
+        border-left: 1px solid var(--default-border);
+        background: transparent;
+        color: var(--default-text);
+        cursor: pointer;
+        transition: all 0.2s ease-in-out;
+        font-size: 1rem;
+        font-weight: 600;
+        border-radius: 0 0.5rem 0.5rem 0;
+
+        &:hover {
+          background-color: var(--default-border);
+        }
+
+        &.active {
+          background-color: var(--default-border) !important;
+          color: var(--highlight);
+        }
+      }
+
+      .current-selected-marker {
+        width: 3px;
+        height: calc(100% / 6);
+        background-color: var(--highlight);
+        position: absolute;
+        left: 0;
+        top: 0;
+        transition: all 0.3s ease-in-out !important;
+        z-index: 10;
       }
     }
 
-    .places-container {
+    & .active-place-content {
+      flex: 1;
       display: flex;
-      justify-content: center;
-      align-items: start;
-      flex-wrap: wrap;
-      gap: 0.5rem;
-      margin-top: 2rem;
-      
+      flex-direction: column;
+      padding: 0 2rem;
+      height: 350px;
 
-      & .buttons-container {
+      & .title {
+        font-size: 1.5rem;
+        font-weight: 600;
+
+        & .place-name {
+          color: var(--default-text);
+          font-weight: 600;
+          text-decoration: none;
+          transition: all 0.2s ease-in-out;
+
+          &::after {
+            content: '';
+            position: absolute;
+            width: 100%;
+            transform: scaleX(0);
+            height: 3px;
+            bottom: 0px;
+            left: 0;
+            background: var(--default-text);
+            transform-origin: bottom right;
+            transition: transform 0.25s ease-out;
+          }
+
+          &:hover::after {
+            transform: scaleX(1);
+            transform-origin: bottom left;
+          }
+        }
+      }
+
+      & .period {
+        font-size: 1rem;
+        color: var(--default-text);
+        margin-bottom: 1rem;
+      }
+
+      & .description {
+        font-size: 1rem;
+        color: var(--default-text);
+        line-height: 1.5rem;
+        text-align: justify;
+        max-width: 800px;
         display: flex;
         flex-direction: column;
-        
+        gap: 0.5rem;
+        list-style-type: circle;
+      }
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  #career {
+    .places-container {
+      display: flex;
+      flex-direction: column;
+
+      & .buttons-container {
+        flex-direction: row;
+        width: 100%;
+
         button {
-          margin: 0;
-          margin-left: 1px;
-          padding: 1rem;
-          border: 0;
-          border-left: 1px solid var(--default-border);
-          background: transparent;
-          color: var(--default-text);
-          cursor: pointer;
-          transition: all 0.2s ease-in-out;
-          font-size: 1rem;
-          font-weight: 600;
-          border-radius: 0 .5rem .5rem 0;
-
-          &:hover {
-            background-color: var(--default-border);
-          }
-
-          &.active {
-            background-color: var(--default-border) !important;
-            color: var(--highlight);
-          }
+          width: calc(100% / 6);
+          border: none;
+          border-top: 1px solid var(--default-border);
+          padding: 0.5rem 0.25rem;
+          border-radius: 0 0 0.5rem 0.5rem;
         }
+      }
 
-        .current-selected-marker {
-          width: 3px;
-          height: 20%;
-          background-color: var(--highlight);
-          position: absolute;
-          left: 0;
-          top: 0;
-          transition: all 0.3s ease-in-out;
-          z-index: 10;
-        }
+      & .current-selected-marker {
+        height: 3px !important;
+        width: calc(100% / 6) !important;
       }
 
       & .active-place-content {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        padding: 0 2rem;
-        height: 350px;
+        padding: 0 !important;
 
-        & .title {
-          font-size: 1.5rem;
-          font-weight: 600;
-          
-          & .place-name {
-            color: var(--default-text);
-            font-weight: 600;
-            text-decoration: none;
-            transition: all 0.2s ease-in-out;
-
-            &::after {
-              content: '';
-              position: absolute;
-              width: 100%;
-              transform: scaleX(0);
-              height: 3px;
-              bottom: 0px;
-              left: 0;
-              background: var(--default-text);
-              transform-origin: bottom right;
-              transition: transform 0.25s ease-out;
-            }
-
-            &:hover::after {
-              transform: scaleX(1);
-              transform-origin: bottom left;
-            }
-          }
+        .title,
+        .period {
+          text-align: center;
         }
+      }
 
-        & .period {
-          font-size: 1rem;
-          color: var(--default-text);
-          margin-bottom: 1rem;
-        }
-
-        & .description {
-          font-size: 1rem;
-          color: var(--default-text);
-          line-height: 1.5rem;
-          text-align: justify;
-          max-width: 800px;
-          display: flex;
-          flex-direction: column;
-          gap: 0.5rem;
-          list-style-type: circle;
-        }
+      ul {
+        padding: 0.5rem;
       }
     }
   }
-
-  @media (max-width: 768px) {
-    #career {
-      .places-container {
-        display: flex;
-        flex-direction: column;
-
-        & .buttons-container {
-          flex-direction: row;
-          width: 100%;
-
-          button {
-            width: 20%;
-            border: none;
-            border-top: 1px solid var(--default-border);
-            padding: 0.5rem 0.25rem;
-            border-radius: 0 0 .5rem .5rem;
-          }
-        }
-
-        & .current-selected-marker {
-          height: 3px !important;
-          width: 20% !important;
-        }
-
-        & .active-place-content {
-          padding: 0 !important;
-
-          .title, .period {
-            text-align: center;
-          }	
-        }
-
-        ul {
-          padding: .5rem;
-        }
-      }
-    }
-  }
+}
 </style>
